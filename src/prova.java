ef836ea024af72d583f9a3e3d287234abff31480
Sara <Sara>
2020-07-10 10:26:09 +0200

    prima commit del progetto

---
+++ src/prova.java
@@ -0,0 +1,16 @@
+
+public class prova {
+	
+	private String stringone;
+
+	public String getStringone() {
+		return stringone;
+	}
+
+	public void setStringone(String stringone) {
+		this.stringone = stringone;
+	}
+	
+	
+
+}
